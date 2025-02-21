3c3f89942f17d2a4ab077aaa3026bbfb07aa3df4
dennisp12umbc <gimpedosrs+1@gmail.com>
2025-02-20 23:17:16 -0500

    add some inventory function
    - Stock different foods + vial
    - Button to reset inventory

--- js/script.js
+++ js/script.js
@@ -1,18 +1,21 @@
 window.onload = function() {
 
-    var mainCanvas;
-    var mainContext;
+    var backgroundCanvas;
+    var backgroundContext;
 
-    var inventoryCanvas;
-    var inventoryContext;
+    var menuAnimationCanvas;
+    var menuAnimationContext;
 
-    var animationCanvas;
-    var animationContext;
+    var inventoryBackgroundCanvas;
+    var inventoryBackgroundContext;
+
+    var inventoryAnimationCanvas;
+    var inventoryAnimationContext;
 
     var ping;
-    var direction = 'North';
-    var highlight = 'No Highlight';
 
+
+    const reset_button = document.getElementById("reset_button");
 
     ping = document.getElementById('ping').value;
 
@@ -28,13 +31,11 @@ window.onload = function() {
     const directionDropdown = document.getElementById("direction");
     directionDropdown.onchange = ( () => {
         updateBackground(directionDropdown.value, highlightDropdown.value);
-        direction = directionDropdown.value;
     });
 
     const highlightDropdown = document.getElementById("highlight");
     highlightDropdown.onchange = ( () => {
         updateBackground(directionDropdown.value, highlightDropdown.value);
-        highlight = highlightDropdown.value;
     });
 
 
@@ -48,19 +49,34 @@ window.onload = function() {
     const rightClickMenuMeat = new Image();
     const rightClickMenuCancel = new Image();
 
+    const tofu = new Image();
+    const worms = new Image();
+    const meat = new Image();
+    const horn = new Image();
+    const vial = new Image();
+
     const redClick = new Image();
     const yellowClick = new Image();
     const background = new Image();
 
 
-    mainCanvas = document.getElementById("mainCanvas");
-    mainContext = mainCanvas.getContext("2d");
+    backgroundCanvas = document.getElementById("backgroundCanvas");
+    backgroundContext = backgroundCanvas.getContext("2d");
 
-    inventoryCanvas = document.getElementById("inventoryCanvas");
-    inventoryContext = inventoryCanvas.getContext("2d");
+    menuAnimationCanvas = document.getElementById("menuAnimationCanvas");
+    menuAnimationContext = menuAnimationCanvas.getContext("2d");
 
-    animationCanvas = document.getElementById("animationCanvas");
-    animationContext = animationCanvas.getContext("2d");
+    inventoryBackgroundCanvas = document.getElementById("inventoryBackgroundCanvas");
+    inventoryBackgroundContext = inventoryBackgroundCanvas.getContext("2d");
+
+    inventoryAnimationCanvas = document.getElementById("inventoryAnimationCanvas");
+    inventoryAnimationContext = inventoryAnimationCanvas.getContext("2d");
+
+
+
+    // inventoryBackgroundContext.strokeStyle = 'black';
+    // inventoryBackgroundContext.lineWidth = 2;
+    // inventoryBackgroundContext.strokeRect(0, 0, inventoryBackgroundCanvas.width, inventoryBackgroundCanvas.height);
 
 
     rightClickMenu.src = "./assets/healermenu.png";
@@ -73,6 +89,12 @@ window.onload = function() {
     rightClickMenuMeat.src = "./assets/healermenumeat.png";
     rightClickMenuCancel.src = "./assets/healermenucancel.png";
 
+    tofu.src = "./assets/poisontofu.png";
+    worms.src = "./assets/poisonworms.png";
+    meat.src = "./assets/poisonmeat.png";
+    horn.src = "./assets/healerhorn.png";
+    vial.src = "./assets/healingvial.png";
+
     redClick.src = "./assets/red_click.gif";
     yellowClick.src = "./assets/yellow_click.gif";
     background.src = "./assets/Backgrounds/No Highlight/North.png"
@@ -81,7 +103,7 @@ window.onload = function() {
     let imgWidth = 222, imgHeight = 142;
     let imageDrawn = false;
 
-    let highlightedOption = "none"; // options are: none, stock up, vial, tofu, worms, walk here, examine, meat, cancel
+    let highlightedOption = "stock up"; // options are: none, stock up, vial, tofu, worms, walk here, examine, meat, cancel
     const redXOptions = ["stock up", "vial", "tofu", "worms", "examine", "meat"];
 
 
@@ -95,19 +117,19 @@ window.onload = function() {
         background.src = path;
 
         background.onload = function () {
-            mainContext.clearRect(0, 0, mainCanvas.width, mainCanvas.height);
-            mainContext.drawImage(background, 0, 0);
+            backgroundContext.clearRect(0, 0, backgroundCanvas.width, backgroundCanvas.height);
+            backgroundContext.drawImage(background, 0, 0);
     
-            mainContext.beginPath();
-            mainContext.moveTo(dispenserClickbox[direction][0]['x'], dispenserClickbox[direction][0]['y']);
+            backgroundContext.beginPath();
+            backgroundContext.moveTo(dispenserClickbox[direction][0]['x'], dispenserClickbox[direction][0]['y']);
             for (let i = 1; i < dispenserClickbox[direction].length; i++) {
-                mainContext.lineTo(dispenserClickbox[direction][i]['x'], dispenserClickbox[direction][i]['y']);
+                backgroundContext.lineTo(dispenserClickbox[direction][i]['x'], dispenserClickbox[direction][i]['y']);
             }
-            mainContext.closePath();
+            backgroundContext.closePath();
     
-            // mainContext.strokeStyle = "red";
-            // mainContext.lineWidth = 0.8;
-            // mainContext.stroke();
+            // backgroundContext.strokeStyle = "red";
+            // backgroundContext.lineWidth = 0.8;
+            // backgroundContext.stroke();
         }
     }
 
@@ -155,44 +177,197 @@ window.onload = function() {
 
 
 
+    const rows = 7;
+    const cols = 4;
+    var inventory = Array.from({ length: rows }, () => Array(cols).fill(""));
+    inventory[0][0] = 'h';
 
-    mainCanvas.addEventListener("mousedown", (event) => {
+    // Values to cycle through
+    const fillValues = ['t', 'w', 'm'];
+    let fillIndex = 0; // Keeps track of which value to insert next
 
-        const rect = animationCanvas.getBoundingClientRect();
+    function hasV() {
+        return inventory.some(row => row.includes('v'));
+    }
+
+    function stock_up() {    
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                if (inventory[i][j] === "") {
+                    if (!hasV()) {
+                        inventory[i][j] = 'v'; // First inserted value is 'v'
+                    } else {
+                        inventory[i][j] = fillValues[fillIndex];
+                        fillIndex = (fillIndex + 1) % fillValues.length; // Cycle through 't', 'w', 'm'
+                    }
+                }
+            }
+        }
+        fillIndex = 0;
+        console.table(inventory); // Display the grid
+    }
+
+    function take_vial() {
+        if (!hasV()) {
+            for (let i = 0; i < rows; i++) {
+                for (let j = 0; j < cols; j++) {
+                    if (inventory[i][j] === "") {
+                        inventory[i][j] = 'v';
+                        break;
+                    }
+                }
+                if (hasV()) break;
+            }
+        }
+        console.table(inventory);
+    }
+
+    function take_tofu() {
+        let num_food_given = 0;
+
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                if (inventory[i][j] === "") {
+                    inventory[i][j] = 't';
+
+                    num_food_given++;
+                    if (num_food_given === 5) break;
+                }
+            }
+            if (num_food_given === 5) break;
+        }
+        console.table(inventory);
+    }
+
+    function take_worms() {
+        let num_food_given = 0;
+
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                if (inventory[i][j] === "") {
+                    inventory[i][j] = 'w';
+
+                    num_food_given++;
+                    if (num_food_given === 5) break;
+                }
+            }
+            if (num_food_given === 5) break;
+        }
+        console.table(inventory);
+    }
+
+    function take_meat() {
+        let num_food_given = 0;
+
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                if (inventory[i][j] === "") {
+                    inventory[i][j] = 'm';
+
+                    num_food_given++;
+                    if (num_food_given === 5) break;
+                }
+            }
+            if (num_food_given === 5) break;
+        }
+        console.table(inventory);
+    }
+
+
+    function drawInventory() {
+        inventoryAnimationContext.clearRect(0, 0, inventoryAnimationCanvas.width, inventoryAnimationCanvas.height);
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                let x = 40 + j * 42;
+                let y = 45 + i * 36;
+                if (inventory[i][j] === 't') {
+                    inventoryAnimationContext.drawImage(tofu, x, y);
+                }
+                else if (inventory[i][j] === 'w') {
+                    inventoryAnimationContext.drawImage(worms, x, y);
+                }
+                else if (inventory[i][j] === 'm') {
+                    inventoryAnimationContext.drawImage(meat, x, y);
+                }
+                else if (inventory[i][j] === 'h') {
+                    inventoryAnimationContext.drawImage(horn, x, y);
+                }
+                else if (inventory[i][j] === 'v') {
+                    inventoryAnimationContext.drawImage(vial, x, y);
+                }
+            }
+        }
+    }
+    
+
+
+    backgroundCanvas.addEventListener("mousedown", (event) => {
+
+        const rect = menuAnimationCanvas.getBoundingClientRect();
         const mouseX = event.clientX - rect.left;
         const mouseY = Math.floor(event.clientY - rect.top);
 
-        if (event.button === 0) {
+        if (event.button === 0) { // left click
 
-            animationContext.clearRect(0, 0, animationCanvas.width, animationCanvas.height);
+            menuAnimationContext.clearRect(0, 0, menuAnimationCanvas.width, menuAnimationCanvas.height);
+
+            console.log(highlightedOption);
 
             if (imageDrawn === true) {
+
+                if (highlightedOption === "stock up") {
+                    stock_up();
+                }
+                else if (highlightedOption === "vial") {
+                    take_vial();
+                }
+
+                else if (highlightedOption === "tofu") {
+                    take_tofu();
+                }
+
+                else if (highlightedOption === "worms") {
+                    take_worms();
+                }
+
+                else if (highlightedOption === "meat") {
+                    take_meat();
+                }
+
+
                 if (redXOptions.includes(highlightedOption)) {
                     playRedXAnimation(event);
                 }
                 else if (highlightedOption === "walk here") {
                     playYellowXAnimation(event);
                 }
+
+                drawInventory();
             }
-            else if (mainContext.isPointInPath(mouseX, mouseY)) {
+
+            else if (backgroundContext.isPointInPath(mouseX, mouseY)) {
+                stock_up();
                 playRedXAnimation(event);
+                drawInventory();
             }
 
             imageDrawn = false;
+            highlightedOption = "stock up";
+
         }
 
 
-        else if (event.button === 2) {
+        else if (event.button === 2) { // right click
 
             if (imageDrawn) return;
 
 
-            if (mainContext.isPointInPath(mouseX, mouseY)) {
+            if (backgroundContext.isPointInPath(mouseX, mouseY)) {
                 imageX = event.clientX - rect.left - 110; // 110 is half the width of the right click menu
                 imageY = Math.floor(event.clientY - rect.top);
         
-                animationContext.clearRect(0, 0, animationCanvas.width, animationCanvas.height);
-                animationContext.drawImage(rightClickMenu, imageX, imageY);
+                menuAnimationContext.clearRect(0, 0, menuAnimationCanvas.width, menuAnimationCanvas.height);
+                menuAnimationContext.drawImage(rightClickMenu, imageX, imageY);
                 highlightedOption = "none";
                 
                 imageDrawn = true;
@@ -202,92 +377,107 @@ window.onload = function() {
 
 
 
-    mainCanvas.addEventListener("mousemove", (event) => {
+    backgroundCanvas.addEventListener("mousemove", (event) => {
 
         if (!imageDrawn) return;
 
-        const rect = animationCanvas.getBoundingClientRect();
+        const rect = menuAnimationCanvas.getBoundingClientRect();
         const mouseX = event.clientX - rect.left;
         const mouseY = Math.floor(event.clientY - rect.top);
 
         const mouseisOutsideMenu = mouseX < imageX - 10 || mouseX > imageX + imgWidth + 10 || mouseY < imageY - 10 || mouseY > imageY + imgHeight + 10;
 
         if (mouseisOutsideMenu) {
-            animationContext.clearRect(0, 0, animationCanvas.width, animationCanvas.height);
+            menuAnimationContext.clearRect(0, 0, menuAnimationCanvas.width, menuAnimationCanvas.height);
             imageDrawn = false;
             return;
         }
 
 
-        animationContext.clearRect(0, 0, animationCanvas.width, animationCanvas.height);
+        menuAnimationContext.clearRect(0, 0, menuAnimationCanvas.width, menuAnimationCanvas.height);
 
         const menuMouseX = mouseX - imageX;
         const menuMouseY = mouseY - imageY;
 
         if (menuMouseX >= -10 && menuMouseX <= -1) {
-            animationContext.drawImage(rightClickMenu, imageX, imageY);
+            menuAnimationContext.drawImage(rightClickMenu, imageX, imageY);
             highlightedOption = "none";
         }
         else if (menuMouseX >= 222 && menuMouseX <= 232) {
-            animationContext.drawImage(rightClickMenu, imageX, imageY);
+            menuAnimationContext.drawImage(rightClickMenu, imageX, imageY);
             highlightedOption = "none";
         }
 
         if (menuMouseY >= -10 && menuMouseY <= -1) {
-            animationContext.drawImage(rightClickMenu, imageX, imageY);
+            menuAnimationContext.drawImage(rightClickMenu, imageX, imageY);
             highlightedOption = "none";
         }
-        else if (menuMouseY >= 142 && menuMouseY <= 152) {
-            animationContext.drawImage(rightClickMenu, imageX, imageY);
+        else if (menuMouseY >= 141 && menuMouseY <= 152) {
+            menuAnimationContext.drawImage(rightClickMenu, imageX, imageY);
             highlightedOption = "none";
         }
 
 
         if (menuMouseX >= 1 && menuMouseX <= 221) {
             if (menuMouseY >= 0 && menuMouseY <= 20) { // Choose Option
-                animationContext.drawImage(rightClickMenu, imageX, imageY);
+                menuAnimationContext.drawImage(rightClickMenu, imageX, imageY);
                 highlightedOption = "none";
             }
             else if (menuMouseY >= 21 && menuMouseY <= 35) { // Stock Up
-                animationContext.drawImage(rightClickMenuStockUp, imageX, imageY);
+                menuAnimationContext.drawImage(rightClickMenuStockUp, imageX, imageY);
                 highlightedOption = "stock up";
             }
             else if (menuMouseY >= 36 && menuMouseY <= 50) { // Vial
-                animationContext.drawImage(rightClicKMenuVial, imageX, imageY);
+                menuAnimationContext.drawImage(rightClicKMenuVial, imageX, imageY);
                 highlightedOption = "vial";
             }
             else if (menuMouseY >= 51 && menuMouseY <= 65) { // Tofu
-                animationContext.drawImage(rightClickMenuTofu, imageX, imageY);
+                menuAnimationContext.drawImage(rightClickMenuTofu, imageX, imageY);
                 highlightedOption = "tofu";
             }
             else if (menuMouseY >= 66 && menuMouseY <= 80) { // Worms
-                animationContext.drawImage(rightClickMenuWorms, imageX, imageY);
+                menuAnimationContext.drawImage(rightClickMenuWorms, imageX, imageY);
                 highlightedOption = "worms";
             }
             else if (menuMouseY >= 81 && menuMouseY <= 95) { // Walk Here
-                animationContext.drawImage(rightClickMenuWalkHere, imageX, imageY);
+                menuAnimationContext.drawImage(rightClickMenuWalkHere, imageX, imageY);
                 highlightedOption = "walk here";
             }
             else if (menuMouseY >= 96 && menuMouseY <= 110) { // Examine
-                animationContext.drawImage(rightClickMenuExamine, imageX, imageY);
+                menuAnimationContext.drawImage(rightClickMenuExamine, imageX, imageY);
                 highlightedOption = "examine";
             }
             else if (menuMouseY >= 111 && menuMouseY <= 125) { // Meat
-                animationContext.drawImage(rightClickMenuMeat, imageX, imageY);
+                menuAnimationContext.drawImage(rightClickMenuMeat, imageX, imageY);
                 highlightedOption = "meat";
             }
             else if (menuMouseY >= 126 && menuMouseY <= 140) { // Cancel
-                animationContext.drawImage(rightClickMenuCancel, imageX, imageY);
+                menuAnimationContext.drawImage(rightClickMenuCancel, imageX, imageY);
                 highlightedOption = "cancel";
             }
         }
+    });
 
+
+    // inventoryBackgroundCanvas.addEventListener("click", (event) => {
+    //     console.log("invy click");
+    // });
+
+
+    reset_button.addEventListener("mousedown", (event) => {
+        if (event.button === 0) {
+            
+            inventory = Array.from({ length: rows }, () => Array(cols).fill(""));
+            inventory[0][0] = 'h';
+
+            drawInventory();
+        }
     });
 
 
     document.addEventListener("contextmenu", (event) => {
         event.preventDefault();
-    })
+    });
 
 }
 
